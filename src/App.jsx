a1c6433204491498dfc1c3df391a6aa4365266b4
AbeloByte <gobeztech20@gmail.com>
2024-09-18 15:09:30 -0400

    Caousel Effect created and added in Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 
 import "./App.css";
 import Header from "./components/Header/Header";
+import CarouselEffect from "./components/Carousel/CarouselEffect";
 
 function App() {
   return (
     <>
       <Header />
+      <CarouselEffect/>
     </>
   );
 }
