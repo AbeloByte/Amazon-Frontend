09539a984625280bd8700d9c49ada3297cd3bf48
AbeloByte <gobeztech20@gmail.com>
2024-09-19 22:36:55 -0400

    Landing Page is Added by substitiuting and covering the Other pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 
 import "./App.css";
 import Header from "./components/Header/Header";
-import CarouselEffect from "./components/Carousel/CarouselEffect";
+import Landing from "./pages/Landing/Landing";
+
+
 
 function App() {
   return (
     <>
       <Header />
-      <CarouselEffect/>
+      <Landing/>
+     
     </>
   );
 }
