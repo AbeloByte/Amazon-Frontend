e53c8806e5ac3012f77883b3a86b6174fa2e83b7
AbeloByte <gobeztech20@gmail.com>
2024-09-22 07:11:39 -0400

    Product detail with some other pages

---
+++ src/pages/Orders/Orders.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+function Orders() {
+  return (
+    <Layout>
+      <div>Orders</div>
+    </Layout>
+  );
+}
+
+export default Orders;
---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+function Payment() {
+  return (
+    <Layout>
+      <div>Payment</div>
+    </Layout>
+  );
+}
+
+export default Payment;
