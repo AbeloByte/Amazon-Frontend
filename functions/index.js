d9b38cba8a4d713cee062c9ddd4f414ca729d766
AbeloByte <gobeztech20@gmail.com>
2024-09-24 14:34:34 -0400

    some initiation in Function FOLDER

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,32 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+import { onRequest } from "firebase-functions/v2/https";
+import logger from "firebase-functions/logger";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import stripe from "stripe";
+
+dotenv.config()
+const stripeE = new stripe(process.env.STRIPE_KEY)
+const app = express()
+// app.use(cors(origin:true))
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:"siccessfull"
+    })
+})
+
+
+
+app.listen(4500,()=>{
+    console.log("server is running")
+})
+
 
 
 
