4ef23781bc8df8b39c81b49b89e9ead198366efd
AbeloByte <gobeztech20@gmail.com>
2024-09-27 14:13:43 -0400

    Modification

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -6,12 +6,14 @@ import { Link, useNavigate } from "react-router-dom";
 import classes from "./Auth.module.css";
 import { auth } from "../../Utility/firebase";
 import Type from "../../Utility/action.type";
+
+
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { DataContext } from "../../components/DataProvider/DataProvider";
-import { useNavigation } from "react-router-dom";
+// import { useNavigation } from "react-router-dom";
 
 import { ClipLoader } from "react-spinners";
 function Auth() {
@@ -19,47 +21,57 @@ function Auth() {
   const [email, setEmail] = useState("");
   // password
   const [password, setPassword] = useState("");
-  // console.log(email, password);
+  console.log( password);
   // error
   const [error, setError] = useState("");
+
+
   //loading
   const [loading, setLoading] = useState({ signIn: false, signUp: false });
   //user
   const [{ user }, dispatch] = useContext(DataContext);
+
+
   // navigation
   const navigate = useNavigate();
 
 
   // console.log(user);
-  const authHandler = (e) => {
+  const authHandler = async (e) => {
     e.preventDefault();
-    // console.log(e.target.name);
+    console.log(e.target.name);
     if (e.target.name === "signin") {
+
+
       setLoading({ ...loading, signIn: true });
+
+      
       signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           // Signed in
           // console.log(userCredential)
 
-          dispatch({
-            type: Type.SET_USER,
-            user: userCredential.user,
-          });
+          // When the user signs in, we update the user state in the context API
+          // with the user that was just signed in. This is done by dispatching
+          // an action of type SET_USER with the new user as the payload.
+          // This will cause the user state to be updated throughout the app.
           setLoading({ ...loading, signIn: false });
 
           navigate("/");  
         })
         .catch((error) => {
-          console.log(error);
+          // console.log(error);
           setError(error.message);
           setLoading({ ...loading, signUp: false });
         });
-    } else {
+    } 
+    
+    else {
       setLoading({ ...loading, signUp: true });
       createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           // Signed up
-          // console.log(userCredential)
+          console.log(userCredential)
           dispatch({
             type: Type.SET_USER,
             user: userCredential.user,
@@ -73,6 +85,10 @@ function Auth() {
         });
     }
   };
+
+
+
+
 
   return (
     <>
@@ -91,9 +107,9 @@ function Auth() {
           <h1>Sign In</h1>
 
           <form action="">
+            {/* Email */}
             <div>
               <label htmlFor="email">Email:</label>
-
               <input
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -101,6 +117,7 @@ function Auth() {
                 id="email"
               />
             </div>
+            {/* password */}
             <div>
               <label htmlFor="password">Password:</label>
 
@@ -111,6 +128,7 @@ function Auth() {
                 id="password"
               />
             </div>
+            {/* Sign In BTN */}
             <button
               type="submit"
               name="signin"
@@ -124,7 +142,6 @@ function Auth() {
               )}
             </button>
             {/* Service agreement */}
-
             <p>
               By signing-in you agree to the{" "}
               <span style={{ fontWeight: "semibold", color: "black" }}>
@@ -133,8 +150,7 @@ function Auth() {
               Conditions of Use & Sale. Please see our Privacy Notice, our
               Cookies Notice and our Interest-Based Ads Notice.
             </p>
-
-            {/* Create new account */}
+            {/* Create new account Btn*/}
             <button
               type="submit"
               name="signup"
@@ -147,6 +163,7 @@ function Auth() {
                 "Create your Amazon account"
               )}
             </button>
+            {/* Error */}
             {error && (
               <small style={{ paddingTop: "16px", color: "red" }}>
                 {error}
@@ -157,68 +174,6 @@ function Auth() {
       </section>
     </>
   );
-}
-// const [isLogin, setIsLogin] = useState(true);
-
-//   // Toggle between Sign In and Sign Up
-//   const toggleForm = () => {
-//     setIsLogin(!isLogin);
-//   };
-
-// return (
-//   <>      <section className={styles.login}>
-//        {/* Logo */}
-//        <Link>
-//          <img
-//           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1206px-Amazon_logo.svg.png"
-//           alt=""
-//         />
-//       </Link>
-//     </section>
-//   <div className={styles.container}>
-//     <h1>{isLogin ? 'Sign In to Your Account' : 'Create Account'}</h1>
-
-//     {isLogin ? (
-//       <form className={styles.authForm}>
-//         <label>Email:</label>
-//         <input type="email" placeholder="Enter your email" required />
-
-//         <label>Password:</label>
-//         <input type="password" placeholder="Enter your password" required />
-
-//         <button type="submit" className={styles.btn}>Sign In</button>
-
-//         <p>
-//           By continuing, you agree to Amazons Conditions of Use and Privacy Notice.
-//         </p>
-//         <hr />
-//         <button onClick={toggleForm} className={styles.toggleBtn}>
-//           Create your Amazon account
-//         </button>
-//       </form>
-//     ) : (
-//       <form className={styles.authForm}>
-//         <label>Name:</label>
-//         <input type="text" placeholder="Enter your name" required />
-
-//         <label>Email:</label>
-//         <input type="email" placeholder="Enter your email" required />
-
-//         <label>Password:</label>
-//         <input type="password" placeholder="Enter your password" required />
-
-//         <label>Re-enter Password:</label>
-//         <input type="password" placeholder="Re-enter your password" required />
-
-//         <button type="submit" className={styles.btn}>Create Account</button>
-//         <hr />
-//         <button onClick={toggleForm} className={styles.toggleBtn}>
-//           Already have an account? Sign in
-//         </button>
-//       </form>
-//     )}
-//   </div>
-//   </>
-// );
+};
 
 export default Auth;
